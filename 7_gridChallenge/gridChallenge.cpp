9d390235d61f4792b0c41d576f6b3e69d19c5e17
Kağan Yavuz <kagan.yavuz@hotmail.com>
2024-12-01 20:45:11 +0300

    Create gridChallenge.cpp

--- 7_gridChallenge/gridChallenge.cpp
+++ 7_gridChallenge/gridChallenge.cpp
@@ -13,7 +13,38 @@ string rtrim(const string &);
  */
 
 string gridChallenge(vector<string> grid) {
-
+    int grid_col_size;
+    int grid_row_size = grid.size();
+    char temp;
+    bool isOrdered = 0;
+    string ret_val = "YES";
+    
+    // compare each col in that row 
+    // and change the order if necessary
+    for (int i = 0; i<grid_row_size; i++){
+        grid_col_size = grid[i].size();
+        for (int j = 0; j<grid_col_size-1; j++){
+            isOrdered = grid[i][j]<grid[i][j+1];
+            if (isOrdered==0){
+                temp=grid[i][j];
+                grid[i][j]=grid[i][j+1];
+                grid[i][j+1]=temp;
+            }
+        }
+    }
+    
+    // compare each row in that col
+    for (int i = 0; i<grid_row_size-1; i++){
+        for (int j = 0; j<grid_col_size; j++){
+            isOrdered = grid[i][j]<grid[i+1][j];
+            if (isOrdered==0){
+                ret_val = "NO";
+            }
+        }
+    }
+    
+    return ret_val;
+    //return "hey";
 }
 
 int main()
